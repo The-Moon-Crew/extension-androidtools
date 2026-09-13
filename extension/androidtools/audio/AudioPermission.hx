815b0b27e86355a1742ffeddb596b7c619823598
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-13 11:29:38 -0300

    Add Bluetooth connect permission handling
    Added Bluetooth permission checks and related functions.

--- extension/androidtools/audio/AudioPermission.hx
+++ extension/androidtools/audio/AudioPermission.hx
@@ -12,6 +12,7 @@ class AudioPermission
 	public static final RECORD_AUDIO:String = 'android.permission.RECORD_AUDIO';
 	public static final MODIFY_AUDIO_SETTINGS:String = 'android.permission.MODIFY_AUDIO_SETTINGS';
 	public static final READ_MEDIA_AUDIO:String = 'android.permission.READ_MEDIA_AUDIO';
+	public static final BLUETOOTH_CONNECT:String = 'android.permission.BLUETOOTH_CONNECT';
 
 	public static function isRecordAudioGranted():Bool
 	{
@@ -28,6 +29,16 @@ class AudioPermission
 		return Permissions.isGranted(READ_MEDIA_AUDIO);
 	}
 
+	public static function isBluetoothConnectGranted():Bool
+	{
+		return Permissions.isGranted(BLUETOOTH_CONNECT);
+	}
+
+	public static function isAllAudioGranted():Bool
+	{
+		return Permissions.isGrantedAll([RECORD_AUDIO, MODIFY_AUDIO_SETTINGS, READ_MEDIA_AUDIO]);
+	}
+
 	public static function requestRecordAudio(requestCode:Int = 101):Void
 	{
 		Permissions.requestPermission(RECORD_AUDIO, requestCode);
@@ -38,9 +49,25 @@ class AudioPermission
 		Permissions.requestPermissionsAsync([RECORD_AUDIO], requestCode, onComplete);
 	}
 
-	public static function requestAudioPermissions(requestCode:Int = 102, onGranted:Void->Void, onDenied:Array<String>->Void):Void
+	public static function requestAudioRecordingPermissions(requestCode:Int = 102, onGranted:Void->Void, onDenied:Array<String>->Void):Void
 	{
 		final requiredPermissions:Array<String> = [RECORD_AUDIO, MODIFY_AUDIO_SETTINGS];
 		Permissions.requestPermissionsWithFallback(requiredPermissions, requestCode, onGranted, onDenied);
+	}
+
+	public static function requestMediaAudioPermission(requestCode:Int = 103, onGranted:Void->Void, onDenied:Array<String>->Void):Void
+	{
+		Permissions.requestPermissionsWithFallback([READ_MEDIA_AUDIO], requestCode, onGranted, onDenied);
+	}
+
+	public static function requestFullAudioAccess(requestCode:Int = 104, onGranted:Void->Void, onDenied:Array<String>->Void):Void
+	{
+		final permissions:Array<String> = [RECORD_AUDIO, MODIFY_AUDIO_SETTINGS, READ_MEDIA_AUDIO];
+		Permissions.requestPermissionsWithFallback(permissions, requestCode, onGranted, onDenied);
+	}
+
+	public static function getDeniedAudioPermissions():Array<String>
+	{
+		return Permissions.getDeniedPermissions([RECORD_AUDIO, MODIFY_AUDIO_SETTINGS, READ_MEDIA_AUDIO]);
 	}
 }
